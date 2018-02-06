078c4b52aa031a4d40c4a2d9efed9982785d9e0a
Glitch (future-hawk) <none>
2018-02-06 02:26:30 +0000

    🐧🗽 Checkpoint script.js:293062/498

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var d;
 AFRAME.registerComponent('barchart', {
   schema: {
     csv: {}
@@ -15,18 +16,18 @@ AFRAME.registerComponent('barchart', {
     });
   },
 
-  generate: function (json) {
+  generate: function (data) {
     var el = this.el;
     // default alpha for bars
     var alpha = 0.6;
-    var data = json.map(function(obj) {return obj.value;});
-    var dataText = json.map(function(obj) {return obj.key;});
+    var scaleMap = data.map(function(d) {return d.value;});
+
     // Scale the height of our bars using d3's linear scale
     var hscale = d3.scaleLinear()
-      .domain([0, d3.max(data)])
+      .domain([0, d3.max(scaleMap)])
       .range([0, 3]);
 
-    var color = d3.scaleLinear().domain([0, d3.max(data)])
+    var color = d3.scaleLinear().domain([0, d3.max(scaleMap)])
       .interpolate(d3.interpolateHcl).range(['#ffb3ba', '#bae1ff']);
 
     // Select the current enity object just like an svg
@@ -34,30 +35,29 @@ AFRAME.registerComponent('barchart', {
 
     // we use d3's enter/update/exit pattern to draw and bind our dom elements
     var bars = currentEntity.selectAll('a-box.bar').data(data);
-    console.log(data)
     // we set attributes on our cubes to determine how they are rendered
     bars.enter().append('a-box').classed('bar', true)
       .attr('position',function (d, i) {
       const x =  i * 0.8 - (data.length / 2);
-      const y = hscale(d)/2;
+      const y = hscale(d.value)/2;
       const z = -3
       return x + " " + y + " " + z   
     })
       .attr('width', function(d) { return 0.5; })
       .attr('depth', function(d) { return 0.5; })
-      .attr('height', function(d) { return hscale(d); })
+      .attr('height', function(d) { return hscale(d.value); })
       .attr('opacity', alpha)
-      .attr('color', function(d) { return color(d); })
+      .attr('color', function(d) { return color(d.value); })
       .on("mouseenter", function(d,i) {
         d3.select(this).transition().duration(10)
           .attr('opacity', 0.9);
 
         d3.select(this).append("a-text")
-          .attr('color', color(d))
+          .attr('color', color(d.value))
           .attr('align', 'center')
-          .attr('position', `0 ${(hscale(d) / 2 + 0.5)} 0`)
+          .attr('position', function(d) { return hscale(d) / 2 + 0.5; } )
           .attr('scale', '1 1 1')
-          .attr('value', function(d, i) { return data["Vehicle Name"]; });
+          .attr('value', function(d) { return d.name; });
       })
       .on("mouseleave", function(d,i) {
         d3.select(this).transition().duration(1000)
